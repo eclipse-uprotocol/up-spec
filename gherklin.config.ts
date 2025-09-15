0cc43c8afb7d7cbd3169ffe093be761c57308cef
Kai Hudalla <kai.hudalla@bosch.com>
2025-09-15 17:59:26 +0200

    Add CI job for linting Gherkin feature files

---
+++ gherklin.config.ts
@@ -0,0 +1,10 @@
+export default {
+    featureDirectory: ".",
+    customRulesDirectory: "./custom-rules",
+    rules: {
+        "allowed-tags": "off",
+        "no-trailing-spaces": "error",
+        "new-line-at-eof": "error",
+    },
+    fix: false,
+}
